a4ea8c3cf3cdfc671201ab88c22eb6c2cdaa86cb
shaik-sohail <sohail.shaik@zemosolabs.com>
2022-09-22 11:00:46 +0530

    user details state managed in single useState

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -2,10 +2,18 @@ import React, { useState } from "react";
 import ManOutlinedIcon from "@mui/icons-material/ManOutlined";
 import WomanOutlinedIcon from "@mui/icons-material/WomanOutlined";
 const UserForm = () => {
-  const [name, setName] = useState();
+  const [userData, setUserData] = useState({
+    name: "",
+    age: 0,
+    gender: "",
+  });
+
+  const changeHandler = (e) => {
+    setUserData((prevValues) => {
+      return { ...prevValues, [e.target.name]: e.target.value };
+    });
+  };
   const [valid, setValid] = useState(false);
-  const [age, setAge] = useState(0);
-  const [gender, setGender] = useState();
   return (
     <div>
       <form>
@@ -18,7 +26,7 @@ const UserForm = () => {
               onChange={(e) => {
                 if (/^[a-zA-Z]/.test(e.target.value)) {
                   setValid(true);
-                  setName(e.target.value);
+                  changeHandler(e);
                 } else {
                   setValid(false);
                 }
@@ -31,19 +39,16 @@ const UserForm = () => {
             <div>
               <label>
                 Age:
-                <input
-                  type="number"
-                  name="Age"
-                  onChange={(e) => setAge(e.target.value)}
-                />
+                <input type="number" name="age" onChange={changeHandler} />
               </label>
             </div>
             <div>
               <label>
                 Gender
                 <select
-                  value={gender}
-                  onChange={(e) => setGender(e.target.value)}
+                  name="gender"
+                  value={userData.gender}
+                  onChange={changeHandler}
                 >
                   <option>Pick a choice!</option>
                   <option value="male">Male</option>
@@ -55,21 +60,21 @@ const UserForm = () => {
         ) : null}
       </form>
       <div style={{ paddingTop: "50px" }}>
-        {age > 0 && age < 20 && (
+        {userData.age > 0 && userData.age < 20 && (
           <img
             src="https://images.pexels.com/photos/247502/pexels-photo-247502.jpeg?cs=srgb&dl=pexels-pixabay-247502.jpg&fm=jpg"
             alt="age<20"
             style={{ width: "500px" }}
           />
         )}
-        {age >= 20 && age < 40 && (
+        {userData.age >= 20 && userData.age < 40 && (
           <img
             src="https://nationalzoo.si.edu/sites/default/files/styles/1400_scale/public/animals/sumatrantiger-002.jpg?itok=0LqptSTF"
             alt="20<age<40"
             style={{ width: "500px" }}
           />
         )}
-        {age >= 40 && (
+        {userData.age >= 40 && (
           <img
             src="https://imgnew.outlookindia.com/uploadimage/library/16_9/16_9_5/IMAGE_1663404877.webp"
             alt="age>40"
@@ -78,8 +83,8 @@ const UserForm = () => {
         )}
       </div>
       <div data-testid="gender">
-        {gender &&
-          (gender === "male" ? (
+        {userData.gender &&
+          (userData.gender === "male" ? (
             <ManOutlinedIcon sx={{ fontSize: 100 }} />
           ) : (
             <WomanOutlinedIcon sx={{ fontSize: 100 }} />
